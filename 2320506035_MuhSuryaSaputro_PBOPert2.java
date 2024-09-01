cd5b87568152226d754e49046640a0f9f46254fb
muhsuryasaputro05 <muhsuryasaputro05@gmail.com>
2024-09-02 00:24:12 +0700

    PrakPBO2

---
+++ 2320506035_MuhSuryaSaputro_PBOPert2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.surya.projek1;
+
+/**
+ *
+ * @author muhsu
+ */
+public class SuryaProjek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Muhammad Surya Saputro");
+        System.out.println("NPM    : 2320506035");
+        System.out.println("Alamat : Wonogiri, Jawa Tengah");
+        System.out.println("No HP  : 08572796134");
+    }
+}
